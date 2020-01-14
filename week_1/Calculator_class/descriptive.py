cc66644a72ac3ae346e0866b4a16a4a604861a1b
Brayton Hall <braytonhall@gmail.com>
2020-01-14 15:05:53 -0500

    adding a lot of stuff on tuesday, added the accidental scratch repository

--- week_1/Calculator_class/descriptive.py
+++ week_1/Calculator_class/descriptive.py
@@ -15,34 +15,34 @@ import numpy
 
 class Calculator:
     def __init__(self, data):
-        self._data = data
+        self.data = data
 
     @property
     def length(self):
-        return len(self._data)
+        return len(self.data)
 
     @property
     def mean(self):
-        return sum(self._data) / len(self._data)
+        return sum(self.data) / len(self.data)
 
     @property
     def stand_dev(self):
-        sqrs = [((item - self.mean) ** 2) for item in self._data]
-        return (sum(sqrs) / (len(self._data) - 1)) ** .5
+        sqrs = [((item - self.mean) ** 2) for item in self.data]
+        return (sum(sqrs) / (len(self.data)-1)) ** .5
 
     @property
     def median(self):
-        if len(self._data) % 2 == 0:
-            return ((len(self._data) // 2) + ((len(self._data) // 2) + 1)) / 2
+        if len(self.data) % 2 == 0:
+            return ((len(self.data) // 2) + ((len(self.data) // 2) + 1)) / 2
         else:
-            return sorted(self._data)[(len(self._data) // 2)]
+            return sorted(self.data)[(len(self.data) // 2)]
 
     def add_data(self, nums):
-        self._data.extend(nums)
+        self.data.extend(nums)
 
     def remove_data(self, nums):
         for i in nums:
-            self._data.remove(i)
+            self.data.remove(i)
 
     # @property
     # def stand_dev(self):
@@ -70,3 +70,48 @@ class Calculator:
 #     bc._data.append(10)
 #     print(f'new new mean: {bc.mean}')
 #     print(f'correct new new mean: {sum(bc._data) / len(bc._data)}')
+
+
+
+
+# CALCULATOR VERSION TWO WITHOUT PROPERTY DECORATORS
+
+class Calculator:
+    def __init__(self, data):
+        self.data = data
+        self.mean = self.get_mean()
+        self.length = self.get_length()
+        self.stand_dev = self.get_stand_dev()
+        self.median = self.get_median()
+
+    # @property
+    def get_length(self):
+        return len(self.data)
+
+    # @property
+    def get_mean(self):
+        self.mean = sum(self.data) / len(self.data)
+        return self.mean
+
+    # @property
+    def get_stand_dev(self):
+        sqrs = [((item - self.mean) ** 2) for item in self.data]
+        return (sum(sqrs) / (len(self.data)-1)) ** .5
+
+    # @property
+    def get_median(self):
+        if len(self.data) % 2 == 0:
+            return ((len(self.data) // 2) + ((len(self.data) // 2) + 1)) / 2
+        else:
+            return sorted(self.data)[(len(self.data) // 2)]
+
+    def add_data(self, nums):
+        self.data.extend(nums)
+        self.mean = self.get_mean()
+        self.length = self.get_length()
+
+    def remove_data(self, nums):
+        for i in nums:
+            self.data.remove(i)
+            self.mean = self.get_mean()
+            self.length = self.get_length()
